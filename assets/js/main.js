b40c7b6bbe956b16107b19698d435b394daebcef
Aleksandra Knott <aleksandraknott@gmail.com>
2024-07-03 12:13:33 +0000

    add toggle on body

--- assets/js/main.js
+++ assets/js/main.js
@@ -74,4 +74,43 @@ function handleButtonClick() {
         });
     }
 
-    // Spinner
+    // TOGGLE
+
+    const body = document.querySelector('body');
+    const switchInput = document.querySelector('.js-switch-input');
+
+    switchInput.addEventListener('change', switchInputChecked);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+function switchInputChecked() {
+  const currentTheme = localStorage.getItem('theme');
+  if (currentTheme === Theme.LIGHT) {
+    body.classList.add(Theme.DARK);
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    body.classList.remove(Theme.DARK);
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
+}
+
+function checkedTheme() {
+  const theme = localStorage.getItem('theme');
+
+  if (!theme) {
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
+
+  if (theme === Theme.DARK) {
+    body.classList.add(Theme.DARK);
+    switchInput.checked = true;
+  }
+
+  if (theme === Theme.LIGHT) {
+    body.classList.add(Theme.LIGHT);
+  }
+}
+checkedTheme();
